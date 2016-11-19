375e0e34fb9d2ef688e62e320c8ee98d2f7cbcd8
Anjaneyulu <anjaneyulu.batta@micropyramid.com>
2016-11-19 22:32:56 +0530

    settings changed

--- moviewiki/settings.py
+++ moviewiki/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'moviewiki.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-if DEBUG == False:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
